029960e1864a24759d5b0a7bdb43a95b3f3181ab
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-05-23 14:33:54 -0400

    Update projects.js

--- portfolio/pages/projects.js
+++ portfolio/pages/projects.js
@@ -2,7 +2,6 @@
 import Head from 'next/head';
 import Projects from '../components/Projects';
 import Background from '../components/Background';
-import Particles from '../components/Particles';
 import Link from 'next/link';
 
 export default function ProjectsPage() {
@@ -13,7 +12,6 @@ export default function ProjectsPage() {
       </Head>
       <main className="relative overflow-hidden min-h-screen bg-neutral-950 text-white scroll-smooth">
         <Background />
-        <Particles />
         <div className="absolute top-6 left-6 z-20 text-2xl">
           <Link href="/" className="text-neutral-400 hover:text-white transition">←</Link>
         </div>
