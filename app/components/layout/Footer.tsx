4b9935866b6104cbe863ce1d0af2d02d2d907d8f
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-05-23 14:05:30 -0400

    Delete app directory

--- app/components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="py-8 text-center border-t border-neutral-800 text-sm text-neutral-500">
-      © {new Date().getFullYear()} Kate Baumeister 
-    </footer>
-  );
-}
