d3b3fc346371201f656e9d9cc17dd77da8c8e72f
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-05-23 12:57:30 -0400

    Update Background.js

--- portfolio/components/Background.js
+++ portfolio/components/Background.js
@@ -1,4 +1,3 @@
-// components/Background.js
 export default function Background() {
   return (
     <div className="absolute inset-0 z-0 pointer-events-none overflow-hidden bg-[radial-gradient(circle,rgba(255,255,255,0.04)_1px,transparent_1px)] [background-size:40px_40px]" />
