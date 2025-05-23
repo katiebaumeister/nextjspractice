a9595f29a569484d9f8391bd2a1ec1017d94706f
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-05-23 10:35:09 -0400

    Update index.js

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import Hero from '../components/Hero';
-import Projects from '../components/Projects';
-import Footer from '../components/Footer';
-import Connect from '../components/Connect';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import Connect from './components/Connect';
 
 export default function Home() {
   return (
