ae454182cadcbf72d8c7bc55aea111573c9c9ae9
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-05-23 22:25:09 -0400

    Update tailwind.config.js

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,5 +1,10 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {
       colors: {
@@ -12,5 +17,19 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          /* Hide scrollbar for Chrome, Safari and Opera */
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+          /* Hide scrollbar for IE, Edge and Firefox */
+          '-ms-overflow-style': 'none', // IE and Edge
+          'scrollbar-width': 'none',    // Firefox
+        },
+      });
+    }),
+  ],
 };
