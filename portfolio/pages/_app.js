52ea0e3376fb0d4e6f619a1b39c5cc9b6c8bde2f
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-05-23 22:52:16 -0400

    Update _app.js

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -1,5 +1,7 @@
+// pages/_app.js
 import '../styles/globals.css';
+import 'leaflet/dist/leaflet.css';
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
